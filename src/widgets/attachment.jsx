c96c2bc687e6212e569c2cce32804bb8784daf9e
Vanshdeep Singh <82263082+vanshdeepsingh-coder@users.noreply.github.com>
2024-02-01 14:11:45 +0530

    Update attachment.jsx

--- src/widgets/attachment.jsx
+++ src/widgets/attachment.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage } from 'react-intl'
 
 import FileProgress from './file-progress.jsx';
 import { bytesToHumanSize } from '../lib/strformat.js';
