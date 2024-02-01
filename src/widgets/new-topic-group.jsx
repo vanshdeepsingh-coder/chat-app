9dcf3aaa8ea203ceee90c6219b863076300b49ad
Vanshdeep Singh <82263082+vanshdeepsingh-coder@users.noreply.github.com>
2024-02-01 14:22:07 +0530

    Update new-topic-group.jsx

--- src/widgets/new-topic-group.jsx
+++ src/widgets/new-topic-group.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage } from 'react-intl'
 
 import AvatarCrop from './avatar-crop.jsx';
 import AvatarUpload from './avatar-upload.jsx';
